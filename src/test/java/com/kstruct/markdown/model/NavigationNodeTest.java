86f26c6e0b5926ce56d867fb79804763b79d4549
Matthew Sheppard <msheppard@funnelback.com>
2016-09-29 13:25:56 +1000

    Added unit tests for the isPageAt/isParentOfPageAt methods

--- src/test/java/com/kstruct/markdown/model/NavigationNodeTest.java
+++ src/test/java/com/kstruct/markdown/model/NavigationNodeTest.java
@@ -13,20 +13,47 @@ import com.google.common.jimfs.Configuration;
 import com.google.common.jimfs.Jimfs;
 
 public class NavigationNodeTest {
-	@Test
-	public void testGetHasHtmlPagesBelow() throws IOException {
+    @Test
+    public void testGetHasHtmlPagesBelow() throws IOException {
         FileSystem fs = Jimfs.newFileSystem(Configuration.unix());
         Path root = fs.getPath("/root/input");
         Files.createDirectories(root);
-		
-		Directory directory = new Directory(root, root, Optional.empty());
-		
-		Assert.assertFalse(directory.getHasHtmlPagesBelow());
-		
-		directory.getChildren().add(new MarkdownPage(root.resolve("example.md"), root, Optional.of(directory)));
 
-		Assert.assertTrue(directory.getHasHtmlPagesBelow());
-	}
-	
-	// TODO - Add tests for node.isPageAt(relativeUri) and node.isParentOfPageAt(relativeUri)
+        Directory directory = new Directory(root, root, Optional.empty());
+
+        Assert.assertFalse(directory.getHasHtmlPagesBelow());
+
+        directory.getChildren().add(new MarkdownPage(root.resolve("example.md"), root, Optional.of(directory)));
+
+        Assert.assertTrue(directory.getHasHtmlPagesBelow());
+    }
+
+    @Test
+    public void testPageDetectionForPage() throws IOException {
+        FileSystem fs = Jimfs.newFileSystem(Configuration.unix());
+        Path root = fs.getPath("/root/input");
+        Files.createDirectories(root);
+        
+        NavigationNode n = new MarkdownPage(root.resolve("foo/bar/example.md"), root, Optional.empty());
+        
+        Assert.assertTrue(n.isPageAt("foo/bar/example.html"));
+        Assert.assertFalse(n.isPageAt("f/bar/example.html"));
+        Assert.assertFalse(n.isPageAt("bar/example.html"));
+        Assert.assertFalse(n.isPageAt("foo/bar/index.html"));
+    }
+
+    @Test
+    public void testPageDetectionForDirectory() throws IOException {
+        FileSystem fs = Jimfs.newFileSystem(Configuration.unix());
+        Path root = fs.getPath("/root/input");
+        Files.createDirectories(root);
+        
+        NavigationNode n = new Directory(root.resolve("foo/bar"), root, Optional.empty());
+        
+        Assert.assertTrue(n.isParentOfPageAt("foo/bar/example.html"));
+        Assert.assertFalse(n.isParentOfPageAt("f/bar/example.html"));
+        Assert.assertFalse(n.isParentOfPageAt("bar/example.html"));
+        Assert.assertTrue(n.isParentOfPageAt("foo/bar/index.html"));
+    }
+
 }
