435e402928efc67d303309222b24844958a2eb9d
Matthew Sheppard <matthew.sheppard@gmail.com>
2016-10-09 06:00:38 +1100

    Broke out the navigation link injection code, added a test for it

--- src/main/java/com/kstruct/markdown/templating/MarkdownProcessor.java
+++ src/main/java/com/kstruct/markdown/templating/MarkdownProcessor.java
@@ -49,11 +49,6 @@ import com.kstruct.markdown.utils.PathUtils;
 
 public class MarkdownProcessor {
 
-//    private Parser parser;
-
-    public MarkdownProcessor() {
-    }
-
     public MarkdownProcessorResult process(String markdownContent, List<String> siblingPages, List<String> subCategories) {
         List<Extension> extensions = Arrays.asList(TablesExtension.create());
 
@@ -64,65 +59,7 @@ public class MarkdownProcessor {
         Set<String> linkTargets = new HashSet<>();
 
         // Modify listing headings
-        document.accept(new AbstractVisitor() {
-            @Override
-            public void visit(Heading heading) {
-                MarkdownTextVisitor textVisitor = new MarkdownTextVisitor();
-                heading.accept(textVisitor);
-                String headingName = textVisitor.getText();
-                                
-                // We do some special processing with certain heading names
-                if (headingName.equals("Generated Section - Pages")) {
-                    if (siblingPages.isEmpty()) {
-                        heading.unlink();
-                    } else {
-                        Heading newHeading = new Heading();
-                        newHeading.setLevel(heading.getLevel());
-                        newHeading.appendChild(new Text("Pages"));
-                        heading.insertAfter(newHeading);
-                        heading.unlink(); // Remove the old heading
-                        
-                        BulletList list = new BulletList();
-                        for (String siblingPage : siblingPages) {
-                            ListItem item = new ListItem();
-                            String title = PathUtils.titleForPath(Paths.get(siblingPage));
-                            Link link = new Link(siblingPage, title);
-                            link.appendChild(new Text(title));
-                            item.appendChild(link);
-                            list.appendChild(item);
-                        }
-                        newHeading.insertAfter(list);
-                    }
-                }
-
-                if (headingName.equals("Generated Section - Categories")) {
-                    if (subCategories.isEmpty()) {
-                        heading.unlink();
-                    } else {
-                        Heading newHeading = new Heading();
-                        newHeading.setLevel(heading.getLevel());
-                        newHeading.appendChild(new Text("Categories"));
-                        heading.insertAfter(newHeading);
-                        heading.unlink(); // Remove the old heading
-    
-                        BulletList list = new BulletList();
-                        for (String subCategory : subCategories) {
-                            ListItem item = new ListItem();
-                            String title = PathUtils.titleForPath(Paths.get(subCategory));
-                            Link link = new Link(subCategory + "/" + MarkdownUtils.DIRECTORY_INDEX_FILE_NAME + MarkdownUtils.MARKDOWN_FILE_EXTENSION,
-                                title);
-                            item.appendChild(link);
-                            link.appendChild(new Text(title));
-                            item.appendChild(link);
-                            list.appendChild(item);
-                        }
-                        newHeading.insertAfter(list);
-                    }
-                }
-                
-                visitChildren(heading);
-            }
-        });
+        document.accept(new NavigationLinkInjector(siblingPages, subCategories));
         
         // Fix links to *.md to go to *.html instead
         document.accept(new AbstractVisitor() {
